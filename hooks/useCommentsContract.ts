7b2a2d4f1e5acc558117c1eaadfec16af1f4e5fc
TomiMoreno <tomimoreno03@gmail.com>
2022-03-09 02:09:24 -0300

    Deploy contract to mumbai

--- hooks/useCommentsContract.ts
+++ hooks/useCommentsContract.ts
@@ -30,7 +30,7 @@ const useCommentsContract = () => {
   // We also pass in the signer if there is a signed in wallet, or if there's
   // no signed in wallet then we'll pass in the connected provider.
   const contract = wagmi.useContract({
-    addressOrName: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    addressOrName: "0xA32Bd540980501A1d5F21ea3Dc5b5a47454724d5",
     contractInterface: CommentsContract.abi,
     signerOrProvider: signer.data || provider,
   });
